5c06e6c8bc0e081b21b0292a09edf160f9f557b6
brenoborba <brenoborba99@gmail.com>
2022-05-31 15:46:57 +0200

    Folder management and form validation refactoring

---
+++ js/form_validation.js
@@ -0,0 +1,112 @@
+const registerUserForm = document.getElementById("registerUser")
+const registerNewsletter = document.getElementById("registerNewsletter")
+
+
+registerUserForm.onsubmit = e => {
+    e.preventDefault()
+    let errorFlag = true
+    const nameInput = document.forms['registerUser']['name']
+    const emailInput = document.forms['registerUser']['email']
+    const cpfInput = document.forms['registerUser']['cpf']
+
+    if(!nameInput.value){
+        errorFlag = true
+        nameInput.classList.add('error')
+        let span = nameInput.nextSibling.nextSibling
+        span.innerText = `Insira o nome corretamente!`
+    } else{
+        errorFlag = false
+        let span = nameInput.nextSibling.nextSibling
+        span.innerText = ''
+
+    }
+
+    if(!emailInput.value){
+        errorFlag = true
+        emailInput.classList.add('error')
+        let span = emailInput.nextSibling.nextSibling
+        span.innerText = `Insira o e-mail corretamente!`
+    } else{
+        errorFlag = false
+        let span = emailInput.nextSibling.nextSibling
+        span.innerText = ''
+    }
+
+    if(!cpfInput.value){
+        errorFlag = true
+        cpfInput.classList.add('error')
+        let span = cpfInput.nextSibling.nextSibling
+        span.innerText = `Insira o CPF corretamente!`
+    } else{
+        errorFlag = false
+        let span = cpfInput.nextSibling.nextSibling
+        span.innerText = ''
+    }
+
+
+    const radioButtons = document.querySelectorAll('input[name="sexo"]');
+    const span = document.querySelector('.radioError')
+    let selectedValue
+
+    for (const radioButton of radioButtons){
+        if(radioButton.checked){
+            selectedValue = radioButton.value
+            break
+        }
+    }
+        
+    if(selectedValue){
+        span.classList.remove('.radioError')
+        span.innerText = ''
+
+    } else{
+        errorFlag = true
+        span.classList.add('.radioError')
+        span.innerText = 'Selecione uma das opções!'
+    }
+
+
+    if(!errorFlag){
+        registerUserForm.submit()
+    }
+
+}
+
+
+registerNewsletter.onsubmit = e =>{
+    e.preventDefault()
+    let errorFlag = true
+
+    const nameInput = document.forms['registerNewsletter']['name']
+    const emailInput = document.forms['registerNewsletter']['email']
+
+    if(!nameInput.value){
+        errorFlag = true
+        nameInput.classList.add('error')
+        let span = nameInput.nextSibling.nextSibling
+        span.innerText = `Insira o nome corretamente!`
+    } else{
+        errorFlag = false
+        let span = nameInput.nextSibling.nextSibling
+        span.innerText = ''
+
+    }
+
+    if(!emailInput.value){
+        errorFlag = true
+        emailInput.classList.add('error')
+        let span = emailInput.nextSibling.nextSibling
+        span.innerText = `Insira o e-mail corretamente!`
+    } else{
+        errorFlag = false
+        let span = emailInput.nextSibling.nextSibling
+        span.innerText = ''
+    }
+    
+
+    if(!errorFlag){
+        registerNewsletter.submit()
+    }
+
+
+}
---
+++ js/script.js
@@ -0,0 +1,51 @@
+/* API request for displaying the catalog */
+const showMoreBtn = document.getElementById('showMoreBtn')
+const catalogGrid = document.querySelector('.catalog-grid')
+const secondPage = document.getElementById('secondPage')
+const mobileLayout = window.matchMedia("(max-width: 720px)")
+secondPage.style.display = 'none'
+
+async function showProducts(page) {
+    let productWrapper
+   await fetch(`https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=${page}`)
+   .then(res => res.json())
+   .then(data => data.products.forEach((product) => {
+        productWrapper = document.createElement('div')
+        productWrapper.innerHTML += 
+        `<div class="catalog-item">
+            <div class="display-item">
+                <img src="${product.image}" alt="" class="displayImg"/>
+            </div>
+
+            <div class="product-info">
+                <div class="productName">${product.name}</div>
+
+                <div class="productDescription">
+                    ${product.description}
+                </div>
+
+                <div class="oldPrice">De: <del>R$${product.oldPrice},00</del></div>
+                <div class="price">Por: R$${product.price},00</div>
+                <div class="installments">ou ${product.installments.count}x de R$${product.installments.value}0</div>
+
+                <button class="btn">Comprar</button>
+            </div>
+        </div>`
+
+        catalogGrid.appendChild(productWrapper)
+
+
+        
+    }))
+
+
+    showMoreBtn.onclick = function(){
+        showProducts(2)
+        secondPage.style.display = ''
+        showMoreBtn.style.display = 'none'
+    }
+
+}
+
+showProducts(1)
+
